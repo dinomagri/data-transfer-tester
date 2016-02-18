80cbe08744bfabe186d687fe77e9af1d36cec32d
Dino Magri <dinomagri@gmail.com>
2016-02-18 21:50:25 -0200

    fixed aria2c tool and add support for multiple flows

--- portalsdmz/measureTools/lib/aria2cTool.py
+++ portalsdmz/measureTools/lib/aria2cTool.py
@@ -3,15 +3,14 @@
 import time, subprocess, re, os
 from measureTools.models import aria2cData
 
-def criar_pasta_local(pasta_des, pasta):
-	print "Criando pasta local para receber o arquivo..."
-	cmd = "if [ ! -d " + "/" + pasta_des + "/" + pasta + \
-		" ] ; then mkdir -p " + "/" + pasta_des + "/" + pasta + " ; fi"
+def createLocalFolder(pasta_des, tipo, tamanho):
+	print "\nCriando pasta local para recebimento de arquivos ...\n"
+	cmd = "if [ ! -d " + " /" + pasta_des + "/" + tipo + "/" + tamanho + " ] ; then mkdir -p /" + pasta_des + "/" + tipo + "/" + tamanho + " ; fi"
 	subprocess.check_call(cmd, shell=True)
 
-def remover_pasta_local(pasta_des, tamanho):
-	print "Deletando pasta local"
-	cmd = "rm -rf " + "/" + pasta_des + "/" + tamanho
+def removeLocalFolder(pasta_des, tipo, tamanho):
+	print "\nRemovendo arquivos e pasta local criadas para o recebimento ...\n"
+	cmd = "rm -rf " + "/" + pasta_des + "/" + tipo + "/" + tamanho
 	subprocess.check_call(cmd, shell=True)
 
 def executa_aria2c(comando):
@@ -45,32 +44,20 @@ def saveAria2cResult(velocidade, numero_teste, cenario, erro):
 def aria2cTool(ip_remoto, tamanho, numero_teste, pasta_ori, pasta_des, fluxo, cenario):
 	# esta fazendo o download em vez de upload
 	
-	pasta_temp = "area-teste"
 	tipo = 'aria2c'
-	usuario  = "root"
 	data     = time.strftime('%d/%m %H:%M:%S')
 	erro 	 = ''
 	work_dir = os.getcwd()
-	pasta = tamanho + "/" + str(fluxo)
-
-	# programa
-	print ""
-	print "---------------"
-	print "--- Aria2c ----"
-	print "---------------"
-	print ""
-
+	
 	try:
-		os.chdir(os.path.abspath(os.sep))
-		criar_pasta_local(pasta_des, pasta)
+		#os.chdir(os.path.abspath(os.sep))
+		createLocalFolder(pasta_des, tipo, tamanho)
 
-		comando = "aria2c " + "ftp://" + ip_remoto + "/" + pasta_temp + "/" + tamanho + "_file -d /" + pasta_des + "/" + pasta
-
-		#comando = 'aria2c -d ' + "/" + pasta_des + '/' + pasta + ' -x ' + \
-		#	 str(fluxo) + ' ftp://' + ip_remoto + \
-		#	 '/' + tamanho + '_file'
+		comando = "aria2c " + "-x" + str(fluxo) + " ftp://" + ip_remoto + "/" + tamanho + "_file -d /" + pasta_des + "/" + tipo + "/" + tamanho + "/" + tamanho + "_file_" + str(numero_teste) 
+		print ">>>>>>>>>>>>>>>>>"
+		print comando
 		resultado_aria2c = executa_aria2c(comando);
-		remover_pasta_local(pasta_des, tamanho)
+		removeLocalFolder(pasta_des, tipo, tamanho)
 		os.chdir(work_dir)
 		saveAria2cResult(resultado_aria2c, numero_teste, cenario, erro)
 
