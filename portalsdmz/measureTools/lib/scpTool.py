212d2fde99fcf56a61556162f731dc25f0eee3bc
Dino Magri <dinomagri@gmail.com>
2016-02-18 20:35:03 -0200

    fixed scp throughput metric

--- portalsdmz/measureTools/lib/scpTool.py
+++ portalsdmz/measureTools/lib/scpTool.py
@@ -6,10 +6,20 @@ def createRemoteFolder(pasta_des, tipo, tamanho, usuario, ip_remoto):
 	cmd_ssh = "if [ ! -d " + " /" + pasta_des + "/" + tipo + "/" + tamanho + " ] ; then mkdir -p " + pasta_des + "/" + tipo + "/" + tamanho + " ; fi"
 	subprocess.check_call(['ssh',usuario + "@" + ip_remoto,cmd_ssh])
 
+def createLocalFolder(pasta_des, tipo, tamanho):
+	print "\nCriando pasta local para recebimento de arquivos ...\n"
+	cmd = "if [ ! -d " + " /" + pasta_des + "/" + tipo + "/" + tamanho + " ] ; then mkdir -p /" + pasta_des + "/" + tipo + "/" + tamanho + " ; fi"
+	subprocess.check_call(cmd, shell=True)
+
 def removeRemoteFolder(pasta_des, tipo, tamanho, usuario, ip_remoto):
 	print "\nRemovendo o arquivo remoto\n"
 	cmd_ssh = "rm -rf " + pasta_des + "/" +  tipo + "/" + tamanho 
 	subprocess.check_call(['ssh',usuario + "@" + ip_remoto,cmd_ssh])
+
+def removeLocalFolder(pasta_des, tipo, tamanho):
+	print "\nRemovendo arquivos e pasta local criadas para o recebimento ...\n"
+	cmd = "rm -rf " + "/" + pasta_des + "/" + tipo + "/" + tamanho
+	subprocess.check_call(cmd, shell=True)
 
 def executeScp(cmd):
 	print "\nExecutando o scp\n"
@@ -22,8 +32,11 @@ def filterResult(retorno_scp, tamanho):
 	regex_linha_tempo = 'Transferred:[\w ,.]*'
 	lista_tempo = re.findall(regex_linha_tempo,retorno_scp)
 	resultado['tempo'] = re.findall(regex_tempo,lista_tempo[0])
-	resultado['velocidade'] = "{0:0.1f}".format(convertBits(tamanho)/float(resultado['tempo'][-1]))
+	resultado['velocidade'] = "{0:0.1f}".format(convertMbits(resultado['tempo'][1])/float(resultado['tempo'][-1]))
 	return resultado
+
+def convertMbits(t_bytes):
+	return ((float(t_bytes)/1024)/1024)*8
 
 def convertBits(tamanho):
 	if tamanho == '128M':
@@ -46,17 +59,27 @@ def scpTool(ip_remoto, tamanho, numero_teste, pasta_ori, pasta_des, cenario):
 	descricao_erro = ''
 
 	try:
-		createRemoteFolder(pasta_des, tipo, tamanho, usuario, ip_remoto)
+		# If upload
+		# createRemoteFolder(pasta_des, tipo, tamanho, usuario, ip_remoto)
+
+		# If Download
+		createLocalFolder(pasta_des, tipo, tamanho)
 
 
-	#	cmd = "scp -v " + usuario + "@" + ip_remoto + ":" + pasta_des + "/" + tipo  + "/" + tamanho + "/" + tamanho + "_file " + "/"+ pasta_ori + "/" + tamanho + "_file_" + str(numero_teste)
-	#	print cmd
-
-		cmd = "scp -v " + " /"+ pasta_ori + "/" + tamanho + "_file " + usuario + "@" + ip_remoto + ":" + pasta_des + "/" + tipo  + "/" + tamanho + "/" + tamanho + "_file_" + str(numero_teste)
+		# If Upload
+		#cmd = "scp -v " + " /"+ pasta_ori + "/" + tamanho + "_file " + usuario + "@" + ip_remoto + ":" + pasta_des + "/" + tipo  + "/" + tamanho + "/" + tamanho + "_file_" + str(numero_teste)
 	
+		# If Download
+		cmd = "scp -v " + usuario + "@" + ip_remoto + ":" + "/" + pasta_ori + "/" + tamanho + "_file" + " /" + pasta_des + "/" + tipo  + "/" + tamanho + "/" + tamanho + "_file_" + str(numero_teste)
 		retorno_scp = executeScp(cmd)
+	
 		resultado_scp = filterResult(retorno_scp, tamanho)
-		removeRemoteFolder(pasta_des, tipo, tamanho, usuario, ip_remoto)
+		
+		#If Upload 
+		# removeRemoteFolder(pasta_des, tipo, tamanho, usuario, ip_remoto)
+		#If Download
+		
+		removeLocalFolder(pasta_des, tipo, tamanho)
 		saveScpResult(resultado_scp['velocidade'], cenario, descricao_erro, numero_teste)
 	
 	except Exception as e:
