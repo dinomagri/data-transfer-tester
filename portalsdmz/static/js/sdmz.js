a48235ecc332e728dc8275d097ff39f9570ec501
DESKTOP-1BTF5RL\showr <rodrigo.gtejos@gmail.com>
2018-10-04 14:24:42 -0300

    update form.py

--- portalsdmz/static/js/sdmz.js
+++ portalsdmz/static/js/sdmz.js
@@ -13,6 +13,7 @@
 //     return false;
 //   });
 // });
+/*
 $(document).ready(function($) {
   $('#loadform').on('submit', function(e) { // form when submit action
     //console.log($(this).serialize()); // log for see the form data serialzed which is equal to the one send when submit normally
@@ -50,7 +51,7 @@ $(document).ready(function($) {
   })
 });
 
-
+*/
 
 $(document).ready(function($) {
   $('#loadscenario').on('submit', function(e) { // form when submit action
