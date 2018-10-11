3cfcbe6769d6eecf706d93f1acfcf04c00c39ec4
DESKTOP-1BTF5RL\showr <rodrigo.gtejos@gmail.com>
2018-10-11 18:05:44 -0300

    fix url redirect on results

--- portalsdmz/scenarios/urls.py
+++ portalsdmz/scenarios/urls.py
@@ -3,8 +3,8 @@ from scenarios import views
 
 urlpatterns = patterns('',
 	url(r'^scenario/$', views.newScenario.as_view(), name='new_scenario'),
-	url(r'^scenario/results/$', views.scenarioList.as_view(), name = 'scenario_list'),
-	url(r'^scenario/results/(?P<pk>\d+)$', views.scenarioResults.as_view(), name = 'scenario_results'),
+	url(r'^results/$', views.scenarioList.as_view(), name = 'scenario_list'),
+	url(r'^results/(?P<pk>\d+)$', views.scenarioResults.as_view(), name = 'scenario_results'),
 	url(r'^helper/', views.scenarioHelper.as_view(), name = 'helper'),
 	url(r'^$', views.newScenarioInit.as_view(), name = 'new_scenarioinit'),
 )
