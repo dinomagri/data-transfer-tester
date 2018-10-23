57a4cf03be5155725e7ecb148eb18926f5e87680
DESKTOP-1BTF5RL\showr <rodrigo.gtejos@gmail.com>
2018-10-23 13:27:41 -0300

    fdt

--- portalsdmz/scenarios/views.py
+++ portalsdmz/scenarios/views.py
@@ -181,8 +181,8 @@ class newScenario(generic.FormView):
 									form.fields['iperf'].widget = forms.HiddenInput()
 								if key == 'scp':
 									form.fields['scp'].widget = forms.HiddenInput()
-								if key == 'fdt.jar':
-									form.fields['fdt'].widget = forms.HiddenInput()
+								# if key == 'fdt.jar':
+								# 	form.fields['fdt'].widget = forms.HiddenInput()
 
 			return self.render_to_response(context)
 
@@ -214,8 +214,7 @@ class newScenario(generic.FormView):
 			ip_remoto = IP
 			# print ("ip remoto")
 			# print ip_remoto
-			iperf=wget=gridftp=axel=udr=aria2c=xrootd=scp=False
-			fdt=True
+			iperf=wget=gridftp=axel=udr=aria2c=xrootd=fdt=scp=False
 
 			aria2c		= form.cleaned_data['aria2c']
 			wget		= form.cleaned_data['wget']
